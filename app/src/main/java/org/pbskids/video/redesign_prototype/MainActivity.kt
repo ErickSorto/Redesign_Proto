4631a29ee4033a72773d8bf71a1a6a258a28384b
Erick Sorto <SortoErick33@gmail.com>
2022-07-14 23:54:36 -0400

    Progress on resizing center item

--- app/src/main/java/org/pbskids/video/redesign_prototype/MainActivity.kt
+++ app/src/main/java/org/pbskids/video/redesign_prototype/MainActivity.kt
@@ -1,20 +1,27 @@
 package org.pbskids.video.redesign_prototype
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import dev.chrisbanes.snapper.SnapOffsets
@@ -22,8 +29,8 @@ import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 import org.pbskids.video.redesign_prototype.model.SingleBox
 import org.pbskids.video.redesign_prototype.repository.SingleBoxRepository
 import org.pbskids.video.redesign_prototype.ui.theme.CustomItem
-import org.pbskids.video.redesign_prototype.ui.theme.ItemDimensions
 import org.pbskids.video.redesign_prototype.ui.theme.RedesignPrototypeTheme
+import org.w3c.dom.Text
 
 
 class MainActivity : ComponentActivity() {
@@ -41,49 +48,76 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
-@OptIn(ExperimentalSnapperApi::class)
+@OptIn(ExperimentalSnapperApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun CircularList(
     data: List<SingleBox>,
     modifier: Modifier = Modifier,
-    isEndless: Boolean = true
+    isEndless: Boolean = true,
 ) {
     val listState = rememberLazyListState(
         if (isEndless) Int.MAX_VALUE / 2 else 0
     )
 
     val configuration = LocalConfiguration.current
-    val itemDimensions = ItemDimensions()
-    val screenHeight = configuration.screenHeightDp.dp
+
     val screenWidth = configuration.screenWidthDp.dp
-    val contentPadding = PaddingValues(start = screenWidth/2 - (itemDimensions.itemWidth/2))
+    val contentPadding = PaddingValues()
+
+    val midIndex by remember(listState.firstVisibleItemIndex) {
+        derivedStateOf {
+            listState.layoutInfo.visibleItemsInfo.run {
+                val firstVisibleIndex = listState.firstVisibleItemIndex
+                if (isEmpty()) -1
+                else if(isEndless){
+                    firstVisibleIndex % data.size + 1
+                }
+                else firstVisibleIndex
+            }
+        }
+    }
     BoxWithConstraints {
         LazyRow(
             state = listState,
-            modifier = modifier,
+            modifier = Modifier,
             contentPadding = contentPadding,
-            flingBehavior = rememberSnapperFlingBehavior(listState, SnapOffsets.Start, snapIndex = { _, startIndex, targetIndex ->
+            flingBehavior = rememberSnapperFlingBehavior(listState, SnapOffsets.Center, snapIndex = { _, startIndex, targetIndex ->
                 targetIndex.coerceIn(startIndex - 7, startIndex + 7)
-            }),
+            })
 
-        ) {
+        )
+        {
             items(
                 count = if (isEndless) Int.MAX_VALUE else data.size,
                 itemContent = {
                     val index = it % data.size
-                    CustomItem(data[index])    // item composable
+                    val padding = if (index == midIndex) 13.dp else 1.dp
+                    Box(
+                        Modifier
+                            .background(Color.Gray, RoundedCornerShape(8.dp))
+                            .padding(horizontal = 8.dp, vertical = padding)
+                    ){
+                        CustomItem(data[index])
+                    }
+                    Text(
+                        text = index.toString(),
+                        textAlign = TextAlign.End,
+                        style = MaterialTheme.typography.subtitle1
+                    )
 
-                    //Animated content that makes items smaller when not selected
-
-
-                },
-
+                }
             )
+
+
         }
 
     }
+    Text(
+        text = listState.firstVisibleItemIndex.toString(),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.subtitle1
+    )
+
 }
 
 
@@ -95,5 +129,16 @@ fun DefaultPreview() {
         val data = singleBoxRepository.getAllData()
         CircularList(data = data)
     }
+}
+
+
+
+@ExperimentalAnimationApi
+fun addAnimation(duration: Int = 800): ContentTransform {
+    return slideInVertically(animationSpec = tween(durationMillis = duration)) { height -> height } + fadeIn(
+        animationSpec = tween(durationMillis = duration)
+    ) with slideOutVertically(animationSpec = tween(durationMillis = duration)) { height -> -height } + fadeOut(
+        animationSpec = tween(durationMillis = duration)
+    )
 }
 
